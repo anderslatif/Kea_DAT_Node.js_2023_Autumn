2bd5fa0e1880804faabe69e931184311521fdc76
anderslatif <anderslatif@gmail.com>
2023-08-29 11:46:03 +0200

    Created the withdrawel amount example

--- 01._First_Server/app.js
+++ 01._First_Server/app.js
@@ -9,6 +9,29 @@ app.get("/", (req, res) => {
     res.send({ data: "This is the first request handler" });
 });
 
+app.get("/dog", (req, res) => {
+    console.log(req.params);
+    res.send({ dog: "Woof" });
+});
+
+app.get("/dog/:firstValue/:someOtherValue", (req, res) => {
+    console.log(req.params);
+    console.log(req.params.someOtherValue);
+    res.send({ dog: "Meow" });
+});
+
+let balance = 100;
+app.get("/wallet/:withdrawalAmount", (req, res) => {
+    const withdrawalAmount = req.params.withdrawalAmount;
+    balance -= withdrawalAmount;
+
+    if (balance < 0) {
+        balance += withdrawalAmount;
+        res.send({ message: "You can't withdraw. No more money left."});
+    } else {
+        res.send({ message: `You've withdrawn ${withdrawalAmount}`});
+    }
+});
 
 // 80 http
 // 443 https
