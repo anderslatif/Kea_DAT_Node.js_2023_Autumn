763b464652b05aa99576cf2b7ab60a2199906096
anderslatif <anderslatif@gmail.com>
2023-09-05 10:29:45 +0200

    Implemented the GET requests for the Mountains API

--- 02._Mountains_API/app.js
+++ 02._Mountains_API/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+const mountains = [
+    { id: 1, name: "Mount Fuji", height: 3776 },
+    { id: 2, name: "Killimanjaro", height: 5895 },
+    { id: 3, name: "Himmelbjerget", height: 147 }
+];
+
+
+app.get("/mountains", (req, res) => {
+    res.send({ data: mountains });
+});
+
+app.get("/mountains/:id", (req, res) => {
+    const pathVariableMountainId = Number(req.params.id);
+    if (!pathVariableMountainId) {
+        res.send({ error: "The mountain id must be a number" });
+    } else {
+        const foundMountain = mountains.find((mountain) => mountain.id === pathVariableMountainId);
+
+        res.send({ data: foundMountain });
+    }
+});
 
 
 const PORT = 8080;
