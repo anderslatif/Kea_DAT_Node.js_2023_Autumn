663e8aeff1f889967ad504bb0fde8617b3e66e04
anderslatif <anderslatif@gmail.com>
2023-09-12 09:32:31 +0200

    Solved the mountains API

--- 02._Mountains_API/app.js
+++ 02._Mountains_API/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
-const mountains = [
+app.use(express.json());
+
+let mountains = [
     { id: 1, name: "Mount Fuji", height: 3776 },
     { id: 2, name: "Killimanjaro", height: 5895 },
     { id: 3, name: "Himmelbjerget", height: 147 }
 ];
 
+let currentId = 3;
 
 app.get("/mountains", (req, res) => {
     res.send({ data: mountains });
@@ -15,7 +18,7 @@ app.get("/mountains", (req, res) => {
 app.get("/mountains/:id", (req, res) => {
     const pathVariableMountainId = Number(req.params.id);
     if (!pathVariableMountainId) {
-        res.send({ error: "The mountain id must be a number" });
+        res.status(404).send({ error: "The mountain id must be a number" });
     } else {
         const foundMountain = mountains.find((mountain) => mountain.id === pathVariableMountainId);
 
@@ -23,6 +26,33 @@ app.get("/mountains/:id", (req, res) => {
     }
 });
 
+
+app.post("/mountains", (req, res) => {
+    const newMountain = req.body;
+    newMountain.id = ++currentId;
+    mountains.push(newMountain);
+    res.send({ data: newMountain });
+});
+
+app.patch("/mountains/:id", (req, res) => {
+    let foundIndex = mountains.findIndex((mountain) => mountain.id === Number(req.params.id));
+    if (foundIndex === -1) {
+        res.status(404).send({ error: `Could not find mountain by id ${req.params.id}` });
+    } else {
+        mountains[foundIndex] = { ...mountains[foundIndex], ...req.body, id: Number(req.params.id) };
+        res.send({ data: mountains[foundIndex] });
+    }
+});
+
+app.delete("/mountains/:id", (req, res) => {
+    const foundIndex = mountains.findIndex((mountain) => mountain.id === Number(req.params.id));
+    if (foundIndex === -1) {
+        res.status(404).send({ error: `Could not find mountain by id ${req.params.id}` });
+    } else {
+        mountains.splice(foundIndex, 1);
+        res.send({ data: Number(req.params.id) });
+    }
+});
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
