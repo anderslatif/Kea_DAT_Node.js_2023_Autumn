d5e7c760e175486b3639353da290935f99c00cf3
anderslatif <anderslatif@gmail.com>
2023-09-19 11:47:36 +0200

    Started working with the poke api

--- 05._Pokemon/app.js
+++ 05._Pokemon/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 const app = express();
 
+app.use(express.static("public"));
+
 import path from "path";
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/frontpage.html"));
+    res.sendFile(path.resolve("./public/frontpage.html"));
 });
 
 
