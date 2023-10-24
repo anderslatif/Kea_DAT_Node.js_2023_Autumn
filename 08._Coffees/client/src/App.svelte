0930478f750175ccbb33dcbf59f59063ccd45f3b
anderslatif <anderslatif@gmail.com>
2023-10-24 09:25:19 +0200

    Created routes with Svelte Navigator

--- 08._Coffees/client/src/App.svelte
+++ 08._Coffees/client/src/App.svelte
@@ -1,4 +1,22 @@
 <script>
   	import { Router, Link, Route } from "svelte-navigator";
+    import Home from "./pages/Home/Home.svelte";
+    import CoffeeFacts from "./pages/CoffeeFacts/CoffeeFacts.svelte";
+    import CoffeeOverview from "./pages/CoffeeOverview/CoffeeOverview.svelte";
 
 </script>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/coffeetypes">Coffee Overview</Link>
+    <Link to="/coffeefacts">Coffee Facts</Link>
+  </nav>
+
+  <div>
+    <Route path="/" component={Home}></Route>
+    <Route path="/coffeetypes"><CoffeeOverview /></Route>
+    <Route path="/coffeefacts"><CoffeeFacts /></Route>
+  </div>
+
+</Router>
