2683028ae26b32bc83a428f7d828de41fafeb7e3
anderslatif <anderslatif@gmail.com>
2023-10-31 09:37:43 +0100

    Created the users session router

--- 08._Coffees/server/app.js
+++ 08._Coffees/server/app.js
@@ -5,14 +5,20 @@ import "dotenv/config";
 import express from "express";
 const app = express();
 
-import session from "express-session";
+import helmet from "helmet";
+app.use(helmet());
 
+import session from "express-session";
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
 }));
+
+import usersRouter from "./routers/usersRouter.js";
+app.use(usersRouter);
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
