bc7334ab04ae652401c629cf0f95047e41fb5d59
anderslatif <anderslatif@gmail.com>
2023-11-07 11:57:28 +0100

    Implemented a Mysql example

--- 11._Supermarkets/databases/createDatabase.js
+++ 11._Supermarkets/databases/createDatabase.js
@@ -1,12 +1,14 @@
 import db from "./connection.js";
 
-const isDeleteMode = false;
+const isDeleteMode = process.argv.findIndex((arg) => arg === "delete") === -1 ? false : true;
+
 
 if (isDeleteMode) {
     await db.exec("DROP TABLE IF EXISTS customers;");
     await db.exec("DROP TABLE IF EXISTS supermarkets;");
 }
 
+// Create the tables (DDL)
 await db.exec(`CREATE TABLE IF NOT EXISTS supermarkets (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT CHECK(name IN ('Netto', 'Rema 1000', 'Lidl') ) NOT NULL,
@@ -21,7 +23,7 @@ await db.exec(`CREATE TABLE IF NOT EXISTS customers (
     FOREIGN KEY (supermarket_id) REFERENCES supermarkets (id)
 );`);
 
-// SEED the database
+// SEED the database (DML)
 if (isDeleteMode) {
     await db.run("INSERT INTO supermarkets (name) VALUES ('Netto');");
     await db.run("INSERT INTO supermarkets (name, is_best_in_denmark) VALUES ('Rema 1000', TRUE);");
