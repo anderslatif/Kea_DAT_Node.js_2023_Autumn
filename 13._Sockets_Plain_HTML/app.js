54552cebb051f663de53c62e3681415ef4055cf9
anderslatif <anderslatif@gmail.com>
2023-11-21 09:25:50 +0100

    Created the basic HTML socket example

--- 13._Sockets_Plain_HTML/app.js
+++ 13._Sockets_Plain_HTML/app.js
@@ -11,6 +11,20 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
     console.log("A socket connected", socket.id);
+
+
+    socket.on("client-choose-color", (data) => {
+
+        // sends to all the sockets in the namespace
+        io.emit("server-sent-color", data);
+
+        // sends to all the sockets in the namespace but itself
+        // socket.broadcast.emit("server-sent-color", data);
+
+        // only emits to the socket itself
+        // socket.emit("server-sent-color", data);
+    });
+
 });
 
 const PORT = process.env.PORT || 8080;
